a8f8604c4dd672f66e228d955cfc350466ccd34e
Christopher Burnett <cburnett@twitter.com>
2012-11-07 14:27:53 -0800

    working with richHttp

--- src/main/scala/com/twitter/http_client/App.scala
+++ src/main/scala/com/twitter/http_client/App.scala
@@ -1,25 +1,26 @@
 package com.twitter.http_client
 
 import com.twitter.finagle.builder.ClientBuilder
-import com.twitter.finagle.http.Http
+import com.twitter.finagle.http.{Response, Request, RichHttp, Http}
 import org.jboss.netty.handler.codec.http._
 import com.twitter.finagle.Service
 import com.twitter.util.Future
 
 object App {
   def main(args: Array[String]) = {
-    val client: Service[HttpRequest, HttpResponse] = ClientBuilder()
-        .codec(Http())
+    val client: Service[Request, Response] = ClientBuilder()
+        .codec(new RichHttp[Request](Http()))
         .hosts("localhost:4567")
         .hostConnectionLimit(1)
         .build()
 
-    val request: DefaultHttpRequest           = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/foo")                      // 2
-    val responseFuture: Future[HttpResponse]  = client(request).onSuccess {
+
+    val request           = Request(HttpVersion.HTTP_1_1, HttpMethod.GET, "/foo")                   // 2
+    val responseFuture: Future[Response]  = client(request).onSuccess {
         response => println("Received response: " + response)
     }
 
-    val body = new String(responseFuture.get().getContent.array)
+    val body = responseFuture.get().contentString
     println("**************************************")
     println(body)
     println("**************************************")
